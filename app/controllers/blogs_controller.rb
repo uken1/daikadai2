90247e425270ebdb3ffd4f4b6db76fb8f214a459
Kenichi Utsumi <k-utsumi@nttpc.co.jp>
2017-05-30 04:32:57 +0000

    createアクションのフラッシュメッセージを変更

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -41,7 +41,7 @@ class BlogsController < ApplicationController
     @blog = Blog.new(blogs_params)
     if @blog.save
      # 一覧画面へ遷移して"ブログを作成しました！"とメッセージを表示します。
-      redirect_to blogs_path, notice: "ブログを作成しました！"
+      redirect_to blogs_path, notice: "ブログを新規作成しました！"
     else
       # エラー発生時に入力フォームを再描画します。
       render 'new'
