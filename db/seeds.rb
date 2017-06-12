4fd51c0948be300603fad15f8eab8353493b6a49
Kenichi Utsumi <k-utsumi@nttpc.co.jp>
2017-06-12 07:41:59 +0000

    9th_commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,16 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 
+100.times do |n|
+  email = Faker::Internet.email
+  name = Faker::Name.name
+  password = "password"
+  User.create!(email: email,
+               password: password,
+               password_confirmation: password,
+               )
+end
+
 n = 1
 while n <= 100
   Blog.create(
